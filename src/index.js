d15566fb116a513a48d743e1e85033fd6b3b65e0
CharlotteM <33549005+DrakLulu@users.noreply.github.com>
2020-08-24 11:24:44 +0200

    many reworks
    App working! :)
    Still need to provide YOUR API key to make it work ( index.js : 17)
    
    React hooks are interesting stuff!

--- src/index.js
+++ src/index.js
@@ -1,44 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { useState, useEffect } from 'react';
 import Json from "./json.json";
 
-// API Key     
 
-function RenderJson(json) {
-  return json.hits.map((element, i) => <img key={i} src={element.webformatURL}></img>);
+function RenderJson(param) {
+  console.log(param.hits);
+  if(param.hits) {
+    return param.hits.map((element) => <img key={element.id} src={element.webformatURL}></img>);
+  }else return <p>No images</p>;
 }
 
-class Columns extends React.Component {
 
-  render() {
-/* get json-images or noting (to handle)
-*/
-      return (
-        <div className="row">
-         {RenderJson(Json)}
-        </div>
-      );
+function useGetData(params) {
+  var API_KEY = '18014975-e304a8e046e40ede228cd6197';
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState("false");
+
+    useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading("true");
+        const response = await fetch(
+          `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(params)}`
+        );
+        const json = await response.json();
+        console.log(json);
+        setResult (json);
+      } catch (error) {
+        setLoading("null");
+      }
     }
+    if (params !== "") {
+      fetchData();
+    }
+  }, [params]);
+  return [result, loading];
+
 }
 
-class Search extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <div className="topnav">
-          <input type="text" id="mySearch" placeholder="Search.." title="search"></input>
-          <button>go!</button>
-        </div>
-            <Columns
-            />
+function App() {
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [result, loading] = useGetData(query);
+  return  (
+    <div>
+      <div className="topnav">
+          <form
+          onSubmit={e => {
+            e.preventDefault();
+            setQuery(search);
+          }}>
+            <input id="text" type="text" onChange={e => setSearch(e.target.value)} />
+            <input type="submit" value="search" />
+          </form>
+        
       </div>
-    );
-  }
+      {loading === "false" ? (<div><p>Search for flowers !</p><div className="row">{RenderJson(Json)}</div></div>
+            ) : (
+                  <div className="row">
+                    {RenderJson(result)}
+                  </div>
+            )}
+    </div>
+  );
 }
 
 ReactDOM.render(
-  <Search />,
-  document.getElementById('root')
+  <App />,
+  document.getElementById('root') 
 );
+
